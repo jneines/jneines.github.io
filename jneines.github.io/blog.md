cf1d4e45f3e4c609fb2635326dbfd336419c8a79
Jens Nie <jneines@web.de>
2024-04-30 14:43:12 +0200

    Optimized update_blog_page tool. Changed design of blog entries

--- jneines.github.io/blog.md
+++ jneines.github.io/blog.md
@@ -5,8 +5,6 @@
 
 :::{grid-item-card} 20240501 - **later**
 :link: https://jneines.github.io/blog/2024/20240501-later/20240501-later.html
-# 20240501 - Later
-
 An incomplete abstract!
 
 
@@ -14,8 +12,6 @@ An incomplete abstract!
 
 :::{grid-item-card} 20240430 - **hello**
 :link: https://jneines.github.io/blog/2024/20240430-hello/20240430-hello.html
-# 202404030 - Hello
-
 An incomplete abstract!
 
 
@@ -23,8 +19,6 @@ An incomplete abstract!
 
 :::{grid-item-card} 20240429 - **earlier**
 :link: https://jneines.github.io/blog/2024/20240429-earlier/20240429-earlier.html
-# 20240429 - Earlier
-
 An incomplete abstract!
 
 
@@ -32,8 +26,6 @@ An incomplete abstract!
 
 :::{grid-item-card} 20231231 - **party_on**
 :link: https://jneines.github.io/blog/2023/20231231-party_on/20231231-party_on.html
-# 20231231 - Party on
-
 End of year!
 
 
@@ -41,8 +33,6 @@ End of year!
 
 :::{grid-item-card} 20230515 - **my_birthday**
 :link: https://jneines.github.io/blog/2023/20230515-my_birthday/20230515-my_birthday.html
-# 20230515 - My Birthday
-
 This is my birthday!
 
 
@@ -50,8 +40,6 @@ This is my birthday!
 
 :::{grid-item-card} 20230101 - **happy_new_year**
 :link: https://jneines.github.io/blog/2023/20230101-happy_new_year/20230101-happy_new_year.html
-# 20230101 - Happy New Year
-
 An incomplete abstract!
 
 
--- tools/update_blog_page.py
+++ tools/update_blog_page.py
@@ -9,8 +9,11 @@ from collections import defaultdict
 
 from loguru import logger
 
+# base url to the blog book living in gh-pages
 online_base_url = "https://jneines.github.io/blog"
+# local base directory for the checked-out homepage repo
 homepage_base_dir = Path("jneines.github.io")
+# local base directory for the checked-out blog repo
 blog_base_dir = Path("../../blog/blog")
 
 # A model to hold all blog items in a structured manner.
@@ -23,29 +26,37 @@ blog_model = defaultdict(list)
 # The toc model is used to create the _toc.yml file later.
 # It will be populated based on the contents of the blog_model
 blog_page_model = [
-        "# Blog",
-        "",
-        "::::{grid} 1",
-        ":gutter: 3",
-        "",
+    "# Blog",  # Title of the overview page
+    "",
+    "::::{grid} 1",  # Structure page using a grid
+    ":gutter: 3",
+    "",
 ]
 
-# find all blog items and add them to the blog_model
-blog_paths = blog_base_dir.glob("20??/20??????-*/abstract.md")
-for blog_path in blog_paths:
-    logger.debug(f"Found {blog_path}")
-    abstract = blog_path.open("r").read()
+# find all abstracts belonging to the blog items and add them to the blog_model
+abstract_paths = blog_base_dir.glob("20??/20??????-*/abstract.md")
+for abstract_path in abstract_paths:
+    logger.debug(f"Found {abstract_path=}")
+
+    abstract = abstract_path.open("r").read()
     logger.debug(f"{abstract=}")
 
-    *_, year_dir, blog_dir, abstract_filename = blog_path.parts
+    *_, year_dir, blog_dir, abstract_filename = abstract_path.parts
     date_str, blog_title = blog_dir.split("-")
     logger.debug(f"{date_str=}, {blog_title=}")
 
-    target_blog_path = blog_path.parent / f"{blog_dir}.html"
+    # The target_blog_path is the document to link to.
+    # Instead of the 'abstract.md' we use the file with the real content.
+    # It's expected that the name of the file is equal to the directory name.
+    target_blog_path = abstract_path.parent / f"{blog_dir}.html"
+
+    # The relative path is the part to append to the 'online_base_url'
+    # to complete the online_url
     rel_path = target_blog_path.relative_to(blog_base_dir)
     online_url = f"{online_base_url}/{rel_path.as_posix()}"
     logger.debug(f"{online_url=}")
 
+    # Add all known to the blog_model
     year_str = year_dir
     blog_model[year_str].append((date_str, blog_title, online_url, abstract))
 
@@ -55,7 +66,10 @@ logger.debug(blog_model)
 # Now iterate the blog_model in reversed order
 
 for year_str in sorted(blog_model.keys())[::-1]:
-    for date_str, blog_title, online_url, abstract in sorted(blog_model[year_str])[::-1]:
+    for date_str, blog_title, online_url, abstract in sorted(blog_model[year_str])[
+        ::-1
+    ]:
+        # Create a clickable 'grid-item-card' for every blog entry
         blog_page_model.append(f":::{{grid-item-card}} {date_str} - **{blog_title}**")
         blog_page_model.append(f":link: {online_url}")
         blog_page_model.append(abstract)
@@ -63,9 +77,11 @@ for year_str in sorted(blog_model.keys())[::-1]:
         blog_page_model.append(":::")
         blog_page_model.append("")
 
+# Finalize the grid
 blog_page_model.append("::::")
 
 logger.debug(f"page contents: {"\n".join(blog_page_model)}")
 
+# Update the blog overview page
 with (homepage_base_dir / "blog.md").open("w") as fd:
     fd.write("\n".join(blog_page_model))
